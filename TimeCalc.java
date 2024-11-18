36c79fa7a4c992f7596db0faf8a16cc207fd45bc
uri15012 <uri.herszenhaut01@post.runi.ac.il>
2024-11-18 13:25:31 +0200

    q1

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,21 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+
+
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt ("" + args [0].charAt(3) + args [0].charAt(4));
+
+        int addedTime = Integer.parseInt (args[1]); // parsing the minutes to add
+
+        int newTime = hours * 60 + minutes + addedTime; // calculating the new time in minutes 
+        minutes = newTime % 60;
+        hours = newTime / 60;
+        hours = hours % 24;
+
+        System.out.printf("%02d:%02d", hours, minutes);
+
+
+        
+       
     }
 }
