18c6b7deb918201dc20aee61d81a8b84820ec7b9
uri15012 <uri.herszenhaut01@post.runi.ac.il>
2024-11-20 20:01:38 +0200

    q3

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,36 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	  String word = args[0];
+          int number = Integer.parseInt(args[1]);
+
+          word = word.toUpperCase();
+          String specialCases = "AEFHILMNORSX";
+          String AN;
+
+                // pegar tamanho da palavra
+          for (int i = 0; i < word.length(); i++) {
+                for (int j = 0; j < specialCases.length(); j++) {
+                        if (word.charAt(i) == specialCases.charAt(j)) { // ve uma por uma
+                                AN = "an";
+                                break;
+                                } else {
+                                AN = "a";      
+                                }
+                                System.out.println("Give me " + AN + word.charAt(i) + ":" + word.charAt (i) + "!");
+                        }
+                }
+                        System.out.println("what does that spell?");
+                
+
+                  for (int k = 0; k < number; k++) { // essa faz com que as palavras se repitam no final
+                                System.out.println(word + "!!!");
+                        }
+                
+
         }
 }
+                
+                
+                
+              
