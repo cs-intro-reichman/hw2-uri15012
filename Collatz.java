76994b3bb34b742797007392182de31fa881c3f9
uri15012 <uri.herszenhaut01@post.runi.ac.il>
2024-11-21 18:44:12 +0200

    5

--- Collatz.java
+++ Collatz.java
@@ -37,7 +37,7 @@ public class Collatz {
 				
 						
 					}
-					System.out.println("Every one of the first " + N + "hailstone sequences reached 1.");
+					System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 
 				}
 			}
